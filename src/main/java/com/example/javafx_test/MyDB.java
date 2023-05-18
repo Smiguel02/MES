a0a73ac2f3c50ceb77f45a00d8cb5131e0dc7e2f
anaRita94 <up201905148@fe.up.pt>
2023-05-18 09:26:58 +0100

    Fixed dependencies

---
+++ src/main/java/com/example/javafx_test/MyDB.java
@@ -0,0 +1,51 @@
+package com.example.javafx_test;
+
+//import model.*;
+//import org.apache.commons.lang3.StringUtils;
+
+import java.sql.*;
+import java.util.*;
+import java.util.jar.JarOutputStream;
+
+
+public class MyDB {
+
+    /* syntax for DB_URL:         jdbc:postgresql://<database_host>:<port>/<database_name> */
+    static final String db_url = "jdbc:postgresql://10.227.240.130:5432/pswa0201";
+
+    static final String user = "pswa0201";
+
+    static final String passwd = "studybud12345";
+
+    private static MyDB single_instance = null;
+
+
+    public MyDB(){}
+
+    // Static method to create instance of Singleton class
+    public static MyDB getInstance() {
+        if (single_instance == null) single_instance = new MyDB();
+        return single_instance;
+    }
+
+    // Declaring a variable of type String
+    public Connection conn = null;
+
+    // Constructor
+    // Here we will be creating private constructor
+    // restricted to this class itself
+
+
+    public Connection connect() throws SQLException {
+        conn = DriverManager.getConnection(db_url, user, passwd);
+        System.out.println(conn);
+        return conn;
+    }
+
+    public void disconnect() throws SQLException {
+        if (conn != null)
+            conn.close();
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.javafx_test {
     requires org.eclipse.milo.opcua.stack.core;
     requires reload4j;
     requires org.eclipse.milo.opcua.sdk.client;
+    requires java.sql;
 
 
     opens com.example.javafx_test to javafx.fxml;
